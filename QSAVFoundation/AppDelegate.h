05373573da0e30efbe00620a928b89450d83af63
zhanghesheng <zhangshuisheng1@126.com>
2017-06-12 07:38:34 +0800

    关于avfoundation的封装优化

---
+++ QSAVFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QSAVFoundation
+//
+//  Created by Bruce on 2017/6/7.
+//  Copyright © 2017年 张水生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QSAVFoundation/Classes/CustomAVCapture.h
@@ -0,0 +1,13 @@
+//
+//  CustomAVCapture.h
+//  QSAVFoundation
+//
+//  Created by Bruce on 2017/6/7.
+//  Copyright © 2017年 张水生. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomAVCapture : NSObject
+
+@end
